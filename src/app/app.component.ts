489cadefd31c6b105c427df34ed1da9b68e6167b
mohammedmassowd@gmail.com <mohammedmassowd@gmail.com>
2025-03-12 04:31:40 +0300

    facebook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
 //import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
-    imports: [ NavbarComponent, SidebarComponent, FooterComponent],
+    imports: [NavbarComponent, SidebarComponent, RouterOutlet,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
